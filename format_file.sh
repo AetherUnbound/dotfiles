42505143a72a8ebd270fd0e4bac28b25a10af08c
Madison Swain-Bowden <bowdenm@spu.edu>
2023-05-26 13:05:58 -0700

    Add format file script for use with PyCharm

---
+++ format_file.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+export PATH=$PATH:~/.local/bin/:~/.nvm/versions/node/v16.19.0/bin/
+
+# get argument
+file="$1"
+# get the extension from the file name
+ext=${file#*.}
+
+case $ext in 
+    md)
+        pnpm exec prettier -w "$file"
+        ;;
+    py)
+        black "$file"
+        ;;
+    *)
+        echo "Can't process file of type '$ext'!"
+        exit 1
+        ;;
+esac
