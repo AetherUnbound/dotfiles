f5959cadfe6c610a0699bd82583581200abd6a4c
Madison Swain-Bowden <madison@aetherunbound.net>
2025-12-27 19:28:17 -0800

    Add ollama reload command

--- programs/openrgb/pre-suspend.sh
+++ programs/openrgb/pre-suspend.sh
@@ -5,5 +5,8 @@ if [ "${1}" == "pre" ]; then
 	/usr/bin/openrgb --config /home/aether/.config/OpenRGB/ -p Off
 elif [ "${1}" == "post" ]; then
 	/usr/bin/openrgb --config /home/aether/.config/OpenRGB/ -p Static
+     # Reload of GPU for ollama
+     # https://github.com/ollama/ollama/blob/main/docs/gpu.mdx#linux-suspend-resume
+     rmmod nvidia_uvm && modprobe nvidia_uvm
 fi
 
